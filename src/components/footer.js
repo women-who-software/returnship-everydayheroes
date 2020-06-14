fdaf170c71108b29b4dd613806efacd6b7eb3482
Dominika Wilk <dwilk8@users.noreply.github.com>
2020-06-14 00:21:45 -0600

    some styling changes, trying to make the site look descent and readable with the form

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import footerStyles from './footer.module.scss'
 
 const Footer = () =>{
     return (
-        <footer> 
+        <footer className={footerStyles.footer}>  
             <p> Made with ♥︎ by the WWCode Boulder/Denver Career Returnship Podcast Team © 2020 </p>
         </footer>
     )
