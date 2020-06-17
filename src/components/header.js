a5db4e969e6c46e1d2b8e07bd573594329a7f4ac
Cara Steffen <cara@treadstonedesign.com>
2020-06-16 19:37:18 -0600

    stashing changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,66 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
- 
-import headerStyles from './header.module.scss'
+import headerStyles from "./header.module.scss"
 
-
-const Header = () =>{
-    return (
-        <header className={headerStyles.header}> 
-            <h1 >   
-            
-                <Link className={headerStyles.title} to='/'>Everyday Heroes Logo</Link>
-            
-            </h1>
-                <nav>
-                    <ul className={headerStyles.nav_list}>
-                        <li> <Link className= {headerStyles.nav_item} activeClassName={headerStyles.active_nav_item} to="/"> Home </Link></li>
-                        <li> <Link className= {headerStyles.nav_item} activeClassName={headerStyles.active_nav_item} to="/about"> About </Link></li>
-                        <li> <Link className= {headerStyles.nav_item} activeClassName={headerStyles.active_nav_item} to="/podcast"> Podcast </Link></li>
-                        <li> <Link className= {headerStyles.nav_item} activeClassName={headerStyles.active_nav_item} to="/contact"> Contact </Link></li>
-                    
-                    
-                    </ul>
-                </nav>
-        </header>
-    )
+const Header = () => {
+  return (
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          Everyday Heroes
+        </Link>
+      </h1>
+      <nav>
+        <ul className={headerStyles.nav_list}>
+          <li>
+            {" "}
+            <Link
+              className={headerStyles.nav_item}
+              activeClassName={headerStyles.active_nav_item}
+              to="/"
+            >
+              {" "}
+              Home{" "}
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              className={headerStyles.nav_item}
+              activeClassName={headerStyles.active_nav_item}
+              to="/about"
+            >
+              {" "}
+              About{" "}
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              className={headerStyles.nav_item}
+              activeClassName={headerStyles.active_nav_item}
+              to="/podcast"
+            >
+              {" "}
+              Podcast{" "}
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              className={headerStyles.nav_item}
+              activeClassName={headerStyles.active_nav_item}
+              to="/contact"
+            >
+              {" "}
+              Contact{" "}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,95 @@
-import React from 'react'
+import React from "react"
 //import {Link} from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from "../components/layout"
 
 // To implement a link to a page do :  <Link to="/contact"> Contact us.</Link>
 
 const IndexPage = () => {
-  return(
+  return (
     <div>
-        <Layout>
-          <h1> Celebrating COVID-19 Heroes</h1>
+      <Layout>
+        <h1> Celebrating COVID-19 Heroes</h1>
 
-          <p> Hello! We're creating a podcast that features Everyday Heroes who are willing to tell 
-              their stories and to share the impact they've made during the COVID-19 pandemic.
-               Fill out this form if you have a story you would like to share. </p>
+        <p>
+          {" "}
+          Hello! We're creating a podcast that features Everyday Heroes who are
+          willing to tell their stories and to share the impact they've made
+          during the COVID-19 pandemic. Fill out this form if you have a story
+          you would like to share.{" "}
+        </p>
 
-              <form name="share-your-story" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="bot-field" />
-                <input type="hidden" name="form-name" value="share-your-story" />
+        <form
+          name="share-your-story"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="share-your-story" />
 
-                  <div className="field half first">
-                    <label htmlFor="name">Full Name</label>
-                    <input type="text" name="name" id="name" placeholder="Jane Smith" />
-                  </div>
+          <div className="field half first">
+            <label htmlFor="name">Full Name</label>
+            <input type="text" name="name" id="name" placeholder="Jane Smith" />
+          </div>
 
-                  <div className="field half">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" placeholder="janesmith@email.com" />
-                  </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="janesmith@email.com"
+            />
+          </div>
 
-                  <div className="field location">
-                    <label htmlFor="email">Location</label>
-                    <input type="text" name="location" id="location" placeholder =" Denver, CO" />
-                  </div>
+          <div className="field location">
+            <label htmlFor="email">Location</label>
+            <input
+              type="text"
+              name="location"
+              id="location"
+              placeholder=" Denver, CO"
+            />
+          </div>
 
-                  <div className="field phone">
-                    <label htmlFor="email">Phone Number </label>
-                    <input type="text" name="phone" id="phone" placeholder="123 456 5555"/>
-                  </div>
+          <div className="field phone">
+            <label htmlFor="email">Phone Number </label>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              placeholder="123 456 5555"
+            />
+          </div>
 
-                  <div>
-                    <label htmlFor="zoom-option ">Are you willing to share your story over zoom?</label>
-                    <select name="zoom-option" id="zoom-option">
-                      <option value="selected" selected></option>
-                      <option value="Yes">Yes</option>
-                      <option value="No">No</option>
-                      <option value="Undecided">Undecided</option>
-                    </select>
-                  </div>
+          <div>
+            <label htmlFor="zoom-option ">
+              Are you willing to share your story over zoom?
+            </label>
+            <select name="zoom-option" id="zoom-option">
+              <option value="selected" selected></option>
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+              <option value="Undecided">Undecided</option>
+            </select>
+          </div>
 
-                  <div className="field">
-                    <label htmlFor="story">Share your story...</label>
-                    <textarea name="story" id="story" rows="6" placeholder="My story..."/>
-                  </div>
+          <div className="field">
+            <label htmlFor="story">Share your story...</label>
+            <textarea
+              name="story"
+              id="story"
+              rows="6"
+              placeholder="My story..."
+            />
+          </div>
 
-                  <div className="actions">
-                      <input type="submit" value="Submit" className="special" />
-                  </div>
-
-            </form>
-          
-
-        </Layout> 
-
+          <div className="actions">
+            <input type="submit" value="Submit" className="special" />
+          </div>
+        </form>
+      </Layout>
     </div>
   )
 }
