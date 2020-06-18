9138286038e7a3a35de4e9430b91868822611ee1
Rafna <rafnabash@gmail.com>
2020-06-17 20:35:16 -0600

    navigate to success page after submitting the form

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { navigate } from "gatsby"
 import * as Yup from "yup";
 //import Alert from 'react-bootstrap/Alert'
 
@@ -8,8 +9,8 @@ import formStyles from './form.module.scss'
 //This RegEx(regular expression) can easily check whether a user has entered something that looks like a valid US phone number.
 const phoneRegExp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
-// Using lib YUP here to validate the rest of the form inputs. 
-// Specifying if a input is required and what message is displayed if validation fails.  
+// Using lib YUP here to validate the rest of the form inputs.
+// Specifying if a input is required and what message is displayed if validation fails.
 const ShareYourStorySchema = Yup.object().shape({
     fullName: Yup.string().required(' Please enter your full name'),
     location: Yup.string().required(' Please enter your location'),
@@ -38,32 +39,34 @@ export default function StoryForm() {
     // use this to test out form locally
     //alert(JSON.stringify(data));
 
-        e.preventDefault();
+        //e.preventDefault();
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: encode({ "form-name": "share-your-story", ...state })
         })
-            .then(response => {
-                setFeedbackMsg(`Thank you for submitting your everyday heroes story. We'll get back to you soon.`)
-                reset()
-                console.log(response)
-              })
+            .then(response => navigate("/success/")
+              //{
+                //setFeedbackMsg(`Thank you for submitting your everyday heroes story. We'll get back to you soon.`)
+                //reset()
+                //console.log(response)
+              //}
+            )
             .catch(error => {
             setFeedbackMsg("Oops, something went wrong. The form could not be submitted. Please try again or send us an email at cv19eh@gmail.com ")
             console.log(error)
             })
-            
+          e.preventDefault();
   };
 
   return (
       <div className={formStyles.row}>
         <div className={formStyles.flex_container}>
 
-            <form onSubmit={handleSubmit(onSubmit)} 
-            name="share-your-story" 
-            method="post" 
-            data-netlify="true" 
+            <form onSubmit={handleSubmit(onSubmit)}
+            name="share-your-story"
+            method="post"
+            data-netlify="true"
             data-netlify-honeypot="bot-field"
             action="/success/">
             <input type="hidden" name="bot-field" aria-label= "Hidden botfield input" />
@@ -77,8 +80,8 @@ export default function StoryForm() {
                 <div className={formStyles.col_50}>
                     <div className="Fullname_area">
                         <label htmlFor="fullName"> Full Name</label>
-                        <input 
-                        type="text" 
+                        <input
+                        type="text"
                         className={
                             !JSON.stringify(formState.touched.fullName)
                             && !errors.fullName ? ""
@@ -86,49 +89,49 @@ export default function StoryForm() {
                               && !errors.fullName ? "is-valid" :"is-invalid"
                           }
                         name="fullName"
-                        id = "fullName" 
-                        placeholder="Jane Smith" 
+                        id = "fullName"
+                        placeholder="Jane Smith"
                         onChange={handleChange}
-                        ref={register} 
+                        ref={register}
                         aria-label="Enter your full name"
                          />
                         {errors.fullName && <p className={formStyles.error_text}>{errors.fullName.message}</p>}
                     </div>
-                    
+
                     <div className="Location_area">
                         <label htmlFor="location">Location</label>
-                        <input 
+                        <input
                         type="text"
                         className={
                             !JSON.stringify(formState.touched.location)
                             && !errors.location ? ""
                             :JSON.stringify(formState.touched.location)
                               && !errors.location ? "is-valid" :"is-invalid"
-                          } 
-                        name="location" 
-                        id="location" 
-                        placeholder = " Denver, CO" 
+                          }
+                        name="location"
+                        id="location"
+                        placeholder = " Denver, CO"
                         onChange={handleChange}
-                        ref={register} 
+                        ref={register}
                         aria-label="Enter your location"
                          />
                         {errors.location && <p className={formStyles.error_text}>{errors.location.message}</p>}
                     </div>
                 </div>
-            
+
                 <div className={formStyles.col_50}>
                     <div className="Email_area">
                         <label htmlFor="email">Email</label>
-                        <input 
+                        <input
                         type="text"
                         className={
                             !JSON.stringify(formState.touched.email)
                             && !errors.email ? ""
                             :JSON.stringify(formState.touched.email)
                               && !errors.email ? "is-valid" :"is-invalid"
-                          } 
-                        name="email" 
-                        placeholder="janesmith@email.com" 
+                          }
+                        name="email"
+                        placeholder="janesmith@email.com"
                         onChange={handleChange}
                         ref={register}
                         aria-label="Enter your email"
@@ -137,18 +140,18 @@ export default function StoryForm() {
                     </div>
                     <div className="Phone_area">
                         <label htmlFor="phone">Phone Number </label>
-                        <input 
+                        <input
                         type="tel"
                         className={
                             !JSON.stringify(formState.touched.phone)
                             && !errors.phone ? ""
                             :JSON.stringify(formState.touched.phone)
                               && !errors.phone ? "is-valid" :"is-invalid"
-                          }  
-                        name="phone" 
-                        placeholder="123 456 5555" 
+                          }
+                        name="phone"
+                        placeholder="123 456 5555"
                         onChange={handleChange}
-                        ref={register} 
+                        ref={register}
                         aria-label= "Enter your phone number"
                         />
                         {errors.phone && <p className={formStyles.error_text}>{errors.phone.message}</p>}
@@ -159,12 +162,12 @@ export default function StoryForm() {
 
                 <div className="Zoom_area">
                     <label htmlFor="zoom_option">Are you willing to share your story over zoom?</label>
-                    <select 
-                    className={formStyles.select_css} 
-                    name="zoom_option" 
-                    id="zoom_option" 
+                    <select
+                    className={formStyles.select_css}
+                    name="zoom_option"
+                    id="zoom_option"
                     defaultValue="undecided"
-                    onChange={handleChange} 
+                    onChange={handleChange}
                     ref={register} >
                         <option value= "undecided" >Undecided</option>
                         <option value="Yes">Yes</option>
@@ -180,19 +183,19 @@ export default function StoryForm() {
                         && !errors.story ? ""
                         :JSON.stringify(formState.touched.story)
                           && !errors.story ? "is-valid" :"is-invalid"
-                      }  
-                    name="story" 
-                    id="story" 
-                    rows="6" 
+                      }
+                    name="story"
+                    id="story"
+                    rows="6"
                     placeholder="My story..."
-                    onChange={handleChange}  
-                    ref={register} 
-                    aria-label= " Enter your everyday hero COVID19 Story" 
+                    onChange={handleChange}
+                    ref={register}
+                    aria-label= " Enter your everyday hero COVID19 Story"
                      />
                     {errors.story && <p className={formStyles.error_text}>{errors.story.message}</p>}
                 </div>
 
-            
+
 
                 <div className="actions">
                 <input type="submit" value="Submit" className="special" aria-label= "submit your share story form" />
@@ -205,4 +208,3 @@ export default function StoryForm() {
     </div>
   );
 }
-
