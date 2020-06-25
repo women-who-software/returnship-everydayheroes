d528d65bd0eb5791cff8fbf4c762c9196d04992b
Rafna <rafnabash@gmail.com>
2020-06-25 15:43:59 -0600

    not much a change in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,36 +1,36 @@
 import React from 'react'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
         <div>
-        
+
             <Layout>
                 <h1> About </h1>
                 <h2> Welcome to Everyday Heroes, a COVID-19 Podcast! </h2>
-                <p>The purpose of this project is to collect and share stories of everyday 
+                <p>The purpose of this project is to collect and share stories of everyday
                 heroes during the COVID-19 pandemic. Click <Link to="/"> here</Link> if you have a story you would like to share.</p>
 
                 <p>Living through a pandemic exacts an emotional toll, which is different for each of us.
-                While it's necessary to understand the facts and statistics during this strange time, it's not sufficient. 
-                We want to understand what people are actually experiencing. While it's important to learn how to protect yourself and your loved ones, 
+                While it's necessary to understand the facts and statistics during this strange time, it's not sufficient.
+                We want to understand what people are actually experiencing. While it's important to learn how to protect yourself and your loved ones,
                 it's also helpful to know that we're not going through this alone.</p>
-                 
-                <p>We believe in the value of sharing human stories. Our hope is that these podcast episodes can help bring us together. 
+
+                <p>We believe in the value of sharing human stories. Our hope is that these podcast episodes can help bring us together.
                 They can paint a picture to reflect our own thoughts and feelings. They can show the actions we took to care for each other along the way.</p>
-                 
+
                 <p>In Season One, we're going to meet an actor who donates blood, a director of a memory care unit, a mask-maker,
-                 a magician who does construction work, a nurse who interacts daily with people who may have been infected, 
+                 a magician who does construction work, a nurse who interacts daily with people who may have been infected,
                  a teacher who lost her job, and several other everyday heroes. Their stories are going to be remembered.</p>
-                 
+
                 We're making this podcast available for free in streaming video and as an audio download. Enjoy!
-                
+
             </Layout>
 
         </div>
     )
 }
 
-export default AboutPage 
+export default AboutPage
