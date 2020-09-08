c31393f329a6a51995ac3a0ed4365c16c81e730c
ambermoreyra <ambermoreyra@gmail.com>
2020-09-08 15:37:48 -0600

    fixed spacing in footer & added link to our website

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,24 +11,17 @@ const Footer = () =>{
             <div className={footerStyles.row}>
                 <div className={footerStyles.left_foot, footerStyles.column}>© Everyday Heroes 2020</div>
                     <div className={footerStyles.center_foot, footerStyles.column}> Follow us 
-                    <Link to="https://www.twitter.com" target="_blank">
-                    <img className={footerStyles.img_sm_logo}
-                        src={TwitterImg}
-                        alt="twitter"/>
-                    </Link>
-
-                    <Link to="https://www.facebook.com/groups/covid19everydayheroes" target="_blank">
-                    <img className={footerStyles.img_sm_logo}
-                        src={FacebookImg}
-                        alt="facebook"/>
-                    </Link>
-                    
+                        <Link to="https://www.facebook.com/groups/covid19everydayheroes" target="_blank">
+                        <img className={footerStyles.img_sm_logo}
+                            src={FacebookImg}
+                            alt="facebook"/>
+                        </Link>
                     </div>
                 <div className={footerStyles.right_foot, footerStyles.column}>
                     <a href="mailto:cv19eh@gmail.com">Contact Us</a>
                 </div>
             </div>
-            <div className={footerStyles.created}>
+            <div className={footerStyles.createdBy}>
                 <a href="https://career-returnship.netlify.app/" target="_blank">Created by WWCode Boulder/Denver Career Returnship</a>
             </div>
         </footer>
