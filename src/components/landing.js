88081ab13c01976692cdb6b092e179910742cfe9
Rafna <rafnabash@gmail.com>
2021-02-21 12:11:12 -0700

    Added image credit to home page

--- src/components/landing.js
+++ src/components/landing.js
@@ -27,32 +27,35 @@ const LandingPage = () =>{
 
         <div className={landingStyles.row }>
           <div className={landingStyles.leftContainer}>
-            
+
             <p>
               {" "}
               Hello! We're creating a podcast that features Everyday Heroes who
               are willing to tell their stories and to share the impact they've
               made during the COVID-19 pandemic. <br /><br />
-              This has been a very difficult 
+              This has been a very difficult
               time for many people. We're not going to shy away from sharing very
-               real problems and tough choices. At the same time, we want to feature 
+               real problems and tough choices. At the same time, we want to feature
                stories that are hopeful and that may inspire others.<br /><br />
 
                 We would like your help. <br /><br />
 
                 We want you to share your stories. While you may not think
-                of yourself as a hero, don't minimize the efforts you've made to help 
+                of yourself as a hero, don't minimize the efforts you've made to help
                 others during these tough days. <br /><br />
-                
+
                 Fill out the form below if you have a story you would like to share with us today.{" "}
             </p>
           </div>
 
-          <div className={landingStyles.rightContainer}> 
-            <Img 
+          <div className={landingStyles.rightContainer}>
+            <Img
             fluid={data.file.childImageSharp.fluid}
             alt=" A women wearing a blue face mask"
             />
+            ©<a href="https://pixabay.com/users/NickyPe-10327513/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5008824">
+            NickyPe </a> via <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5008824">
+            Pixabay</a>
           </div>
 
           </div>
